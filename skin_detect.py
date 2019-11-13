056a976c78458c9ec5255b9cac0c5655a79c0076
Vishwajeet Agrawal <vishwa.grawal@gmail.com>
2019-11-13 17:13:53 +0530

    A

--- skin_detect.py
+++ skin_detect.py
@@ -82,8 +82,10 @@ def mask_nonskin(im_src, mask):
     return uint8((mask//255)*im_src)
 
 def detect_skin(im):
+    print(im.shape)
     start_time = time.time()
     # im = cv2.imread(filename)
+    # im = im1.copy()
     w,h,_ = im.shape
     
     # r,g,b = np.split(im,3,axis=2)
@@ -161,15 +163,11 @@ def get_ROI(im):
     cv2.imshow('marked areas', image)
     cv2.waitKey(0)
 
-if __name__=='__main__':
-    # parser = argparse.ArgumentParser(description = 'Skin Detector')
-    # parser.add_argument('f', metavar = 'stop_front.png', type = str, help = 'Input File')
-    # args = parser.parse_args()
-    # im = cv2.imread(args.f)
-    # vd = cv2.VideoCapture(0)
+def skin_detection_live():
+    vd = cv2.VideoCapture(0)
     ret = True
-    # ret,im = vd.read()
-    ret = False
+    ret,im = vd.read()
+    # ret = False
     while(ret):
         w,h,_ = im.shape
         im = cv2.resize(im,(h//2,w//2))
@@ -181,8 +179,10 @@ if __name__=='__main__':
         filesave = fileroot[0] + '_1.' + fileroot[1]
 
         kernel = np.ones((5, 5), np.uint8)
-        img_dilation = cv2.erode(mask, kernel, iterations=1)
-        img_dilation = cv2.dilate(mask, kernel, iterations=5)
+        vkernel = np.ones((5,1),np.uint8)
+        # img_dilation = cv2.erode(mask, kernel, iterations=3)
+        img_dilation = cv2.dilate(mask, vkernel, iterations=1)
+        img_dilation = cv2.dilate(mask, kernel, iterations=1)
         # cv2.imshow('dilated', img_dilation)
         # cv2.imwrite('undillated.png',mask)
         # cv2.imwrite('dillated.png',img_dilation)
@@ -299,3 +299,157 @@ def pool2d(A, kernel_size, stride, padding, pool_mode='max'):
         return A_w.mean(axis=(1,2)).reshape(output_shape)
     elif pool_mode == 'min':
         return A_w.min(axis=(1,2)).reshape(output_shape)
+
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+import sys
+def object_track(): 
+    # Set up tracker.
+    # Instead of MIL, you can also use
+ 
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    tracker_type = tracker_types[5]
+ 
+    if int(minor_ver) < 3:
+        tracker = cv2.Tracker_create(tracker_type)
+    else:
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        if tracker_type == 'GOTURN':
+            tracker = cv2.TrackerGOTURN_create()
+        if tracker_type == 'MOSSE':
+            tracker = cv2.TrackerMOSSE_create()
+        if tracker_type == "CSRT":
+            tracker = cv2.TrackerCSRT_create()
+ 
+    # Read video
+    video = cv2.VideoCapture(0)
+ 
+    # Exit if video not opened.
+    if not video.isOpened():
+        print("Could not open video")
+        sys.exit()
+ 
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+     
+    # Define an initial bounding box
+    bbox = (287, 23, 86, 320)
+    
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI(frame, False)
+    cv2.destroyAllWindows()
+    print(bbox)
+    # Initialize tracker with first frame and bounding box
+
+    frames_required = 10
+    p1 = (int(bbox[0]), int(bbox[1]))
+    p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+
+    # while(True):
+    #     k = cv2.waitKey(30)
+    #     if (k==32):
+    #         break
+    k =0
+    key_p = 0
+    while (True):
+        k = cv2.waitKey(30)
+        ret, frame = video.read()
+        if k==32:
+            key_p = k
+        
+        if ret: 
+            #  # hand initialization
+            # cv2.imshow()
+            if (key_p==32):
+                # print(p1,p2)
+                fcopy = frame[p1[1]:p2[1],p1[0]:p2[0]].copy()
+                mask,im_masked = detect_skin(fcopy)
+                # print(p1[1],p1[1],p2[0],p2[0])
+                # print(im_masked.shape)
+                frame[p1[1]:p2[1],p1[0]:p2[0]] = im_masked
+                cv2.imshow('H',frame)
+                if (np.average(mask.reshape(mask.shape[0]*mask.shape[1])) > 255*0.6):
+                    if (frames_required > 0):
+                        frames_required -=1
+                        if (frames_required == 5):
+                            # frame = im_masked
+                            # frame[p1[1]:p2[1],p1[0]:p2[0]] = im_masked
+                            break
+                            # cv2.imshow('masked_box',im_masked)
+                        continue
+
+                else:
+                    frames_required = 10
+                    continue
+
+            
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+            # frame = cv2.flip(frame,1)
+            cv2.imshow("Initialize Tracking", cv2.flip(frame,1))
+
+            # k = cv2.waitKey(30)
+            
+                # if (key_p==32):
+                #     break
+    # cv2.destroyAllWindows()
+    cv2.imshow('Hello1',frame)
+    ok = tracker.init(frame, bbox)
+ 
+    while True:
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
+            break
+         
+        # Start timer
+        timer = cv2.getTickCount()
+ 
+        # Update tracker
+        ok, bbox = tracker.update(frame)
+ 
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+ 
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+        else :
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+ 
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2)
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
+
+        # Display result
+        # frame = cv2.flip(frame,1)
+        cv2.imshow("Tracking", frame)
+ 
+        # Exit if ESC pressed
+        k = cv2.waitKey(30) & 0xff
+        if k == 27 : break
+
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description = 'Skin Detector')
+    parser.add_argument('f', metavar = 'stop_front.png', type = str, help = 'Input File')
+    args = parser.parse_args()
+    # im = cv2.imread(args.f)
+    object_track()
+    # skin_detection_live()
