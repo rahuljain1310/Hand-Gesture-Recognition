87731d7a6de3d2fbf428b1f293bfe49a91032c8b
Vishwajeet Agrawal <vishwa.grawal@gmail.com>
2019-11-13 19:43:55 +0530

    A

--- CNN_Architecture.py
+++ CNN_Architecture.py
@@ -6,18 +6,38 @@ import torch.nn.functional as F
 class Net(nn.Module):
   def __init__(self):
     super(Net, self).__init__()
-    self.conv1 = nn.Conv2d(3, 6, 5)
+    self.conv1 = nn.Conv2d(3, 12, 3,padding = 0)
     self.pool = nn.MaxPool2d(2, 2)
-    self.conv2 = nn.Conv2d(6, 16, 5)
-    self.fc1 = nn.Linear(16*9*9, 120)
-    self.fc2 = nn.Linear(120, 84)
-    self.fc3 = nn.Linear(84, 4)
+    self.conv2 = nn.Conv2d(12, 30, 3,padding = 1)
+    self.pool = nn.MaxPool2d(2,2)
+    self.conv3 = nn.Conv2d(30,40,3)
+    self.pool = nn.MaxPool2d(2,2)
+    self.fc1 = nn.Linear(40*5*5,100)
+    self.fc2 = nn.Linear(100,3)
+    # self.conv4 = nn.Conv2d(40,40,)
+    # self.pool = nn.MaxPool2d()
+    # self.fc1 = nn.Linear(16*9*9, 120)
+    # self.fc2 = nn.Linear(120, 84)
+    # self.fc3 = nn.Linear(84, 4)
 
   def forward(self, x):
     x = self.pool(F.relu(self.conv1(x)))
     x = self.pool(F.relu(self.conv2(x)))
-    x = x.view(-1, 16 * 9 * 9)
+    x = self.pool(F.relu(self.conv3(x)))
+    x = x.view(-1, 40*5*5)
     x = F.relu(self.fc1(x))
-    x = F.relu(self.fc2(x))
-    x = self.fc3(x)
+    x = (self.fc2(x))
+    # x = self.fc3(x)
     return x
+
+# class Net(nn.Module):
+#   def __init__(self):
+#     super(Net, self).__init__()
+#     self.conv1 = nn.Conv2d(3, 6, 5)
+#     self.pool = nn.MaxPool2d(2, 2)
+#     self.conv2 = nn.Conv2d(6, 16, 5)
+#     self.fc1 = nn.Linear(16*9*9, 120)
+#     self.fc2 = nn.Linear(120, 84)
+#     self.fc3 = nn.Linear(84, 4)
+  
+#   def 
